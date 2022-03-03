072f1feb6d7b122719eb3a61ea0351e132c33553
James Shin <jddshin@gmail.com>
2022-03-03 11:10:29 -0500

    frontend skeleton commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,75 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { useState } from "react"
+import { Link } from "react-router-dom"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function Index(props) {
+  // state to hold formData
+  const [newForm, setNewForm] = useState({
+    name: "",
+    image: "",
+    title: "",
+  })
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  // handleChange function for form
+  const handleChange = event => {
+    setNewForm({ ...newForm, [event.target.name]: event.target.value })
+  }
+
+  // handle submit function for form
+  const handleSubmit = event => {
+    event.preventDefault()
+    props.createPeople(newForm)
+    setNewForm({
+      name: "",
+      image: "",
+      title: "",
+    })
+  }
+
+  // loaded function
+  const loaded = () => {
+    return props.people.map(person => (
+      <div key={person._id} className="person">
+        <Link to={`/people/${person._id}`}>
+          <h1>{person.name}</h1>
+        </Link>
+        <img src={person.image} alt={person.name} />
+        <h3>{person.title}</h3>
+      </div>
+    ))
+  }
+
+  const loading = () => {
+    return <h1>Loading...</h1>
+  }
+  return (
+    <section>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={newForm.name}
+          name="name"
+          placeholder="name"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          value={newForm.image}
+          name="image"
+          placeholder="image URL"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          value={newForm.title}
+          name="title"
+          placeholder="title"
+          onChange={handleChange}
+        />
+        <input type="submit" value="Create Person" />
+      </form>
+      {props.people ? loaded() : loading()}
+    </section>
+  )
+}
+
+export default Index
